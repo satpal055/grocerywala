a6265b0d7b082bdf426874013bb57e21d28152d7
satpal055 <satpal055singh@gmail.com>
2026-02-02 11:35:03 +0530

    add start script for render deploy

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Product = require("../models/Product");
+const Product = require("../models/product");
 const router = express.Router();
 const multer = require("multer");
 const path = require("path");
